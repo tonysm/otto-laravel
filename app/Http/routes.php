e174af59be53a8dcb76e4ce1f16a72df8cae252d
Tony Messias <tonyzrp@gmail.com>
2015-10-24 11:20:03 -0300

    try different configs

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,17 +1,6 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
 Route::get('/', function () {
-    $count=Redis::incr('count');
+    $count = Redis::incr('count');
     return view('welcome', ['count' => $count]);
 });
