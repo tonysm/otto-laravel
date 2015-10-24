2383ce44032b2ec9338900c31022b72dce5e50b0
Tony Messias <tonyzrp@gmail.com>
2015-10-24 14:27:21 -0300

    fix view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Accesses: {{ $count }}</div>
+                <div class="title">Access count: {{ $count }}</div>
             </div>
         </div>
     </body>
